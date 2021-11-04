074f0a86727f38ad60389af6f11831663351c3f0
David Tunnell <davidtunnell9@gmail.com>
2021-11-04 16:40:40 -0500

    added ability to use redux dev tools in chrome for debugging project

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,6 +2,10 @@ import { createStore } from "redux";
 import allReducers from "./reducers";
 
 //create store is a higher order function that creates an initial store
-const store = createStore(allReducers);
+const store = createStore(
+    allReducers,
+    //allows use of redux dev tools
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
