5d3cd525a2e6748e4e50fa8cb5f9a5854ae81a85
David Tunnell <davidtunnell9@gmail.com>
2021-11-09 20:54:35 -0600

    cleaned up searchbar look

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,9 +1,34 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 const SearchBar = () => {
     return (
         <>
-            <section className="bg-dark p-2">
-                <div className="container">
-                    <div className="row text-white justify-content-between align-items-center">
+            <section className="bg-light p-2">
+                <div className="container ">
+                    <div class="row justify-content-between align-items-center">
+                        <div class="col-md-12">
+                            <div class="input-group rounded">
+                                <input
+                                    class="form-control py-2 border-right-0 border"
+                                    type="search"
+                                    placeholder="Search subjects..."
+                                />
+                                <span
+                                    class="input-group-append"
+                                    style={{
+                                        border: "1px solid #e5e5e5",
+                                        borderRadius: "0.25rem",
+                                    }}
+                                >
+                                    <div class="input-group-text bg-white text-muted">
+                                        <FontAwesomeIcon icon={faSearch} />
+                                    </div>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div className="row text-white justify-content-between align-items-center">
                         <div className="col-md-12">
                             <div className="input-group rounded">
                                 <input
@@ -22,7 +47,7 @@ const SearchBar = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </section>
         </>
