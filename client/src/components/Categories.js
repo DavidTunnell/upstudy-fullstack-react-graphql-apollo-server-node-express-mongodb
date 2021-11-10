a27b6a2e95d8a6a525b8983f4e8985d53f4964c3
David Tunnell <davidtunnell9@gmail.com>
2021-11-10 09:43:14 -0600

    making progress on new subject cards

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -8,7 +8,95 @@ const Categories = () => {
     console.log(subjects);
     return (
         <>
-            <section class="bg-white p-5">
+            <section>
+                <div class="card-columns">
+                    <div
+                        class="card p-3 m-3 text-white text-center bg-success"
+                        // style={{ maxWidth: "400px" }}
+                    >
+                        <img
+                            class="card-img-top"
+                            src="https://upload.wikimedia.org/wikipedia/commons/6/67/Babbage40.png"
+                            alt="Card  cap"
+                        />
+                        <div class="card-body">
+                            <h5 class="card-title">Computer Science</h5>
+                            <p class="card-text">1 sub category</p>
+                        </div>
+                    </div>
+                    <div
+                        class="card p-3 m-3 text-white text-center bg-primary"
+                        // style={{ maxWidth: "400px" }}
+                    >
+                        <img
+                            class="card-img-top"
+                            src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Z3_Deutsches_Museum.JPG"
+                            alt="Card  cap"
+                        />
+                        <div class="card-body">
+                            <h5 class="card-title">Computer Science</h5>
+                            <p class="card-text">1 sub category</p>
+                        </div>
+                    </div>
+                    <div
+                        class="card p-3 m-3 text-white text-center bg-danger"
+                        // style={{ maxWidth: "400px" }}
+                    >
+                        <img
+                            class="card-img-top"
+                            src="https://upload.wikimedia.org/wikipedia/commons/2/21/Euclid.jpg"
+                            alt="Card  cap"
+                        />
+                        <div class="card-body">
+                            <h5 class="card-title">Computer Science</h5>
+                            <p class="card-text">1 sub category</p>
+                        </div>
+                    </div>
+                    <div
+                        class="card p-3 m-3 text-white text-center bg-warning"
+                        // style={{ maxWidth: "400px" }}
+                    >
+                        <img
+                            class="card-img-top"
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/RedCrossNursen.jpg/330px-RedCrossNursen.jpg"
+                            alt="Card  cap"
+                        />
+                        <div class="card-body">
+                            <h5 class="card-title">Computer Science</h5>
+                            <p class="card-text">1 sub category</p>
+                        </div>
+                    </div>
+                    <div
+                        class="card p-3 m-3 text-white text-center bg-info"
+                        // style={{ maxWidth: "400px" }}
+                    >
+                        <img
+                            class="card-img-top"
+                            src="https://upload.wikimedia.org/wikipedia/commons/d/d7/Philippine-stock-market-board.jpg"
+                            alt="Card  cap"
+                        />
+                        <div class="card-body">
+                            <h5 class="card-title">Computer Science</h5>
+                            <p class="card-text">1 sub category</p>
+                        </div>
+                    </div>
+                    <div
+                        class="card p-3 m-3 text-white text-center bg-success"
+                        // style={{ maxWidth: "400px" }}
+                    >
+                        <img
+                            class="card-img-top"
+                            src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Product_life_cycle.png"
+                            alt="Card  cap"
+                        />
+                        <div class="card-body">
+                            <h5 class="card-title">Computer Science</h5>
+                            <p class="card-text">1 sub category</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* <section class="bg-white p-5">
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-md-8 text-center">
@@ -58,9 +146,24 @@ const Categories = () => {
                         </div>
                     </div>
                 </div>
-            </section>
+            </section> */}
         </>
     );
 };
 
 export default Categories;
+
+// <div
+//     class="card p-3 m-3 text-white text-center bg-success"
+//     style={{ maxWidth: "400px" }}
+// >
+//     <img
+//         class="card-img-top"
+//         src="https://upload.wikimedia.org/wikipedia/commons/6/67/Babbage40.png"
+//         alt="Card  cap"
+//     />
+//     <div class="card-body">
+//         <h5 class="card-title">Computer Science</h5>
+//         <p class="card-text">1 sub category</p>
+//     </div>
+// </div>;
