5c47dbd6749c112245e3dde614364a21a10d2008
David Tunnell <davidtunnell9@gmail.com>
2021-11-11 15:52:53 -0600

    search now just filtering by name property

--- client/src/redux/reducers/filteredCategories.js
+++ client/src/redux/reducers/filteredCategories.js
@@ -11,12 +11,12 @@ function filteredCategoriesReducer(state = [], action) {
             const categoriesArray = action.payload.data.categories;
             const userInput = action.payload.data.value;
 
-            //https://stackoverflow.com/questions/44312924/filter-array-of-objects-whose-any-properties-contains-a-value
-            return categoriesArray.filter((o) =>
-                Object.keys(o).some((k) =>
-                    o[k].toLowerCase().includes(userInput.toLowerCase())
-                )
-            );
+            return categoriesArray.filter(function (category) {
+                return category.name
+                    .toLowerCase()
+                    .includes(userInput.toLowerCase());
+            });
+
         default:
             //always return state at the end so the whole app doesn't break if there is an issue
             return state;
