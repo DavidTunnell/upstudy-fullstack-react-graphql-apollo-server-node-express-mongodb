16917ef930d6e29456ac55a4d76ccf2d9550b289
David Tunnell <davidtunnell9@gmail.com>
2021-11-17 14:38:02 -0600

    generic modal now functions properly other than background overlay

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,3 +1,5 @@
+import React, { useEffect } from "react";
+
 const Modal = (params) => {
     //get params passed in when calling modal, these are ultimately derived form redux store
     let showModal = params.show;
@@ -5,35 +7,61 @@ const Modal = (params) => {
     let title = params.title;
     let content = params.content;
     let imageUrl = params.imageUrl;
+
+    useEffect(() => {
+        if (showModal) {
+            var btn = document.getElementById("hidden-open-modal-button");
+            btn.click();
+        }
+    });
+
+    const handleBackdropClick = async (event) => {
+        if (event.target.id === "generic-modal") {
+            console.log("handleBackdropClick");
+            console.log(event.target.id);
+            closeModal();
+            var btn = document.getElementById("close-generic-modal-button");
+            btn.click();
+        }
+    };
+
     return (
         <>
+            <button
+                type="button"
+                id="hidden-open-modal-button"
+                className="invisible"
+                data-toggle="modal"
+                data-target="#generic-modal"
+                data-backdrop="false"
+            ></button>
+            {/* <div
+                className="modal-backdrop fade show"
+                onClick={handleBackdropClick}
+            ></div> */}
             <div
-                className={`modal fade ${showModal ? "show" : ""}`}
-                tabIndex="-1"
+                class="modal fade"
+                id="generic-modal"
+                tabindex="-1"
                 role="dialog"
+                aria-labelledby="generic-modal"
                 aria-hidden="true"
-                style={{ display: `${showModal ? "block" : "none"}` }}
+                onClick={handleBackdropClick}
             >
-                <div
-                    className="modal-dialog modal-dialog-centered"
-                    role="document"
-                >
-                    <div className="modal-content">
-                        <div className="justify-content-end">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header justify-content-end ">
                             <button
                                 type="button"
-                                className="close"
+                                class="close"
+                                id="close-generic-modal-button"
                                 data-dismiss="modal"
                                 aria-label="Close"
-                                onClick={() => closeModal()}
                             >
-                                <span
-                                    aria-hidden="true"
-                                    className="icon-x"
-                                ></span>
+                                <span aria-hidden="true" class="icon-x"></span>
                             </button>
                         </div>
-                        <div className="modal-body text-center">
+                        <div className="modal-body text-center  mt-n3">
                             <h3>{title}</h3>
                             <p>{content}</p>
                             {imageUrl?.toString().length > 0 && (
@@ -45,10 +73,6 @@ const Modal = (params) => {
                     </div>
                 </div>
             </div>
-            <div
-                onClick={() => closeModal()}
-                className={` ${showModal ? "modal-backdrop fade show" : ""}`}
-            ></div>
         </>
     );
 };
