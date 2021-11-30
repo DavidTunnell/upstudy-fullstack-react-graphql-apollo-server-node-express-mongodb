ebd76ca639bedb4d8bb92178a51cea48f5afa9e0
David Tunnell <davidtunnell9@gmail.com>
2021-11-29 21:29:13 -0600

    now using react-share in the modal

--- client/src/components/ShareModal.js
+++ client/src/components/ShareModal.js
@@ -1,4 +1,49 @@
 import React, { useEffect, useState } from "react";
+import {
+    EmailShareButton,
+    FacebookShareButton,
+    FacebookMessengerShareButton,
+    HatenaShareButton,
+    InstapaperShareButton,
+    LineShareButton,
+    LinkedinShareButton,
+    LivejournalShareButton,
+    MailruShareButton,
+    OKShareButton,
+    PinterestShareButton,
+    PocketShareButton,
+    RedditShareButton,
+    TelegramShareButton,
+    TumblrShareButton,
+    TwitterShareButton,
+    WhatsappShareButton,
+} from "react-share";
+
+import {
+    EmailIcon,
+    FacebookIcon,
+    FacebookMessengerIcon,
+    HatenaIcon,
+    InstapaperIcon,
+    LineIcon,
+    LinkedinIcon,
+    LivejournalIcon,
+    MailruIcon,
+    OKIcon,
+    PinterestIcon,
+    PocketIcon,
+    RedditIcon,
+    TelegramIcon,
+    TumblrIcon,
+    TwitterIcon,
+    ViberIcon,
+    VKIcon,
+    WeiboIcon,
+    WhatsappIcon,
+    WorkplaceIcon,
+} from "react-share";
+
+//https://www.npmjs.com/package/react-share
 
 const ShareModal = ({ showModal, setShowModal }) => {
     //get params passed in when calling modal, these are ultimately derived form redux store
@@ -72,7 +117,26 @@ const ShareModal = ({ showModal, setShowModal }) => {
                             </button>
                         </div>
                         <div className="modal-body text-center mt-n3">
-                            Test!@#$%^&*()_+
+                            <FacebookShareButton
+                                url={"https://peing.net/ja/"}
+                                quote={
+                                    "フェイスブックはタイトルが付けれるようです"
+                                }
+                                hashtag={"#hashtag"}
+                                description={"aiueo"}
+                                className="Demo__some-network__share-button"
+                            >
+                                <FacebookIcon size={32} round /> Facebookでshare
+                            </FacebookShareButton>
+                            <br />
+                            <TwitterShareButton
+                                title={"test"}
+                                url={"https://peing.net/ja/"}
+                                hashtags={["hashtag1", "hashtag2"]}
+                            >
+                                <TwitterIcon size={32} round />
+                                Twitterでもshare
+                            </TwitterShareButton>
                         </div>
                     </div>
                 </div>
